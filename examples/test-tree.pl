0e6627422c3244ff75de926e181a219f3a743f31
Richard Kelsch <kelschr@cargotel.com>
2017-03-10 23:12:23 -0700

    Initial copy

---
+++ examples/test-tree.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+# This script demonstrates how Debug::Easy automatically indents output
+# depending on where it is called from in the code.  Here it uses the
+# Pseudo module "X1::X2" to nest further levels beyond main subroutines.
+
+use strict;
+
+use Debug::Easy;
+use X1::X2;
+
+my $D = Debug::Easy->new('LogLevel' => 'DEBUGMAX', 'Color' => 1, 'CPADDING' => -20);
+
+$D->DEBUGMAX(['First a local message']);
+firstlevel();
+secondlevel();
+
+sub firstlevel {
+    $D->DEBUG(['First level']);
+    secondlevel();
+}
+
+sub secondlevel {
+    $D->INFO(['Second level']);
+    X1::X2::thirdlevel($D);
+}
